a92ca7eca5e0fd43567aed0c120760e1f0503bbf
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-10-13 06:42:27 -0300

    Update ec2_check.py

--- ec2_check.py
+++ ec2_check.py
@@ -1,7 +1,12 @@
 import os, boto3
 
 def ec2_check(accountid):
-    
-    os.system("aws ec2 describe-instances --filter 'Name=tag:Environment,Values=Staging' --query 'Reservations[].Instances[].{InstanceId:InstanceId,PrivateIP:PrivateIpAddress,Tag:Tags}'")
 
-    return
+    privateip = ""
+    instacename = ""
+
+    if privateip != "":
+        os.system("aws ec2 describe-instances --filter 'Name=network-interface.addresses.private-ip-address,Values="+privateip+" --query 'Reservations[].Instances[].{InstanceId:InstanceId,PrivateIP:PrivateIpAddress,Tag:Tags}'")
+
+    if  instacename != "":
+        os.system("aws ec2 describe-instances --filter 'Name=tag:Name,Values='"+instacename+" --query 'Reservations[].Instances[].{InstanceId:InstanceId,PrivateIP:PrivateIpAddress,Tag:Tags}'")
