5f2bbb96f4d9618e96483b39981400b4691e20cb
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-10-13 06:49:11 -0300

    13.10.2022

--- ec2_check.py
+++ ec2_check.py
@@ -1,10 +1,21 @@
-import os, boto3
+import os, boto3, json
 
 def ec2_check(accountid):
-
     privateip = ""
     instacename = ""
 
+    organizations = boto3.client('organizations')
+    org_response = organizations.describe_account(
+        AccountId=accountid
+    )
+
+    org_json = json.dumps(org_response, indent=4, sort_keys=True, default=str)
+
+    org_database = json.loads(org_json)
+
+    accountname = org_database['Account']['Name']
+    
+    print(accountid+","+accountname)
     if privateip != "":
         os.system("aws ec2 describe-instances --filter 'Name=network-interface.addresses.private-ip-address,Values="+privateip+" --query 'Reservations[].Instances[].{InstanceId:InstanceId,PrivateIP:PrivateIpAddress,Tag:Tags}'")
 
